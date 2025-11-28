2093ca12fb9336bcff52f39fd33404a4efdd5739
Magooch <crvallecito@gmail.com>
2025-11-27 19:25:33 -0700

    Convert user input to array of integers

--- radices.c
+++ radices.c
@@ -13,12 +13,38 @@ void clear() {
 }
 
 void menu_banner() {
-    printf(" _____           _ _                 _______          _ _    _ _\n");
-    printf("|  __ \\         | (_)               |__   __|        | | |  (_) |\n");
-    printf("| |__) |__ _  __| |_  ___ ___  ___     | | ___   ___ | | | ___| |_\n");
-    printf("|  _  // _` |/ _` | |/ __/ _ \\/ __|    | |/ _ \\ / _ \\| | |/ / | __|\n");
-    printf("| | \\ \\ (_| | (_| | | (_|  __/\\__ \\    | | (_) | (_) | |   <| | |_\n");
-    printf("|_|  \\_\\__._|\\__._|_|\\___\\___||___/    |_|\\___/ \\___/|_|_|\\_\\_|\\__|\n\n");
+    printf(" ___         _ _             _____         _ _   _ _\n");
+    printf("| _ \\__ _ __| (_)__ ___ ___ |_   _|__  ___| | |_(_) |_\n");
+    printf("|   / _` / _` | / _/ -_|_-<   | |/ _ \\/ _ \\ | / / |  _|\n");
+    printf("|_|_\\__,_\\__,_|_\\__\\___/__/   |_|\\___/\\___/_|_\\_\\_|\\__|\n\n");
+}
+
+void display_menu() {
+    printf("Welcome to your Radices Toolkit!\n\n");
+
+    // menu options
+    printf("1. Decimal to Radices Converter\n");
+    printf("2. Radices to Decimal Converter\n");
+    printf("3. Radices Calculator\n\n");
+}
+
+/*
+// FIND LENGTH OF ARRAY
+int length(char* arr) {
+    int len = sizeof(arr) / sizeof(arr[0]);
+
+    return len;
+}
+*/
+
+int return_int(char ch) {
+    int n = 0;
+
+    if (ch >= '0' && ch <= '9') {
+        n = ch - '0';
+    }
+
+    return n;
 }
 
 // DECIMAL TO RADICES CONVERTER
@@ -27,20 +53,37 @@ void decimal_to_radices() {
     clear();
     menu_banner();
     printf("DECIMAL TO RADICES CONVERTER\n");
-    printf("Convert Base 10 to any radix from binary to hexadecimal!\n\n");
+    printf("Convert decimal numbers to radices (binary - hexadecimal)!\n\n");
 
     bool persist = true;
 
     while(persist) {
-        int decimal_num;
+        char decimal[10];
+        char radix[2];
 
         printf("Enter your number: ");
-        scanf("%d", &decimal_num);
-        if (decimal_num > 0) {
-            persist = false;
-        } else {
-            persist = false;
+        scanf("%9s", decimal);
+
+        printf("Enter your radix: ");
+        scanf("%1s", radix);
+
+        int len = 0;
+        for (int i = 0; decimal[i] != '\0'; i++) {
+            len += 1;
         }
+
+        printf("len: %d\n", len);
+
+        int int_arr[len];
+        for (int i = 0; decimal[i] != '\0'; i++) {
+            int_arr[i] = return_int(decimal[i]);
+        }
+
+        for (int i = 0; i < len; i++) {
+            printf("%d\n", int_arr[i]);
+        }
+
+        persist = false;
     }
 
     printf("Coming soon! Program ending...\n");
@@ -51,7 +94,7 @@ void radix_to_decimal() {
     clear();
     menu_banner();
     printf("RADICES TO DECIMAL CONVERTER\n");
-    printf("Convert radices from binary to hexadecimal to decimal!\n\n");
+    printf("Convert radices (binary - hexadecimal) to decimal!\n\n");
 
     bool persist = true;
     while (persist) {
@@ -103,42 +146,40 @@ void radices_calculator() {
     printf("Thank you, calculator coming soon...\n");
 }
 
-// MAIN FUNCTION FOR MAIN MENU
+// MAIN MENU
 int main() {
-    clear();
-
-    // menu banner
-    menu_banner();
-    printf("Welcome to your Radices Toolkit!\n");
-    printf("Choose a tool to use...\n\n");
-
-    // menu options
-    printf("1. Decimal to Radices Converter\n");
-    printf("2. Radices to Decimal Converter\n");
-    printf("3. Radices Calculator\n\n");
 
     // menu functionality
     bool menu_persist = true;
+    bool run_menu = true;
     while (menu_persist) {
-        int num;
-        printf("Choose your option: ");
-        scanf("%d", &num);
-        if (num == 1) {
-            decimal_to_radices();
-            menu_persist = false;
-        } else if (num == 2) {
-            radix_to_decimal();
-            menu_persist = false;
-        } else if (num == 3) {
-            radices_calculator();
-            menu_persist = false;
+        if (run_menu) {
+            clear();
+            menu_banner();
+            display_menu();
         } else {
             clear();
             menu_banner();
-            printf("1. Decimal to Radices Converter\n");
-            printf("2. Radices to Decimal Converter\n");
-            printf("3. Radices Calculator\n\n");
-            printf("Please choose a listed option!\n");
+            display_menu();
+            printf("Incorrect choice\n");
+        }
+        char choice[16];
+        printf("Choose your tool (1-3): ");
+        scanf("%15s", choice);
+        if (strcmp(choice, "1") == 0) {
+            decimal_to_radices();
+            menu_persist = false;
+        } else if (strcmp(choice, "2") == 0) {
+            radix_to_decimal();
+            menu_persist = false;
+        } else if (strcmp(choice, "3") == 0) {
+            radices_calculator();
+            menu_persist = false;
+        } else if (strcmp(choice, "exit") == 0) {
+            return EXIT_SUCCESS;
+        } else {
+            run_menu = false;
+            printf("test\n");
         }
     }
 
